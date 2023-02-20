f932ca6c8ef6fae934c2c085060ef61e4a3408cc
Marcelo Távora <marcelovtneves@gmail.com>
2023-02-20 16:51:13 -0300

    ➕ Implementado tailwind vite e primereact

---
+++ src/Pages/Login/index.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export const LoginPage = () => {
+  return (
+    <div className="h-screen flex justify-center items-center">
+      <div className="bg-red-500"></div>
+    </div>
+  );
+};
