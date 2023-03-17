10ffaab8bd7c4322e3b131a83653caf9c05f0531
Marcelo Távora <marcelovtneves@gmail.com>
2023-03-17 17:52:56 -0300

    ✨ Feito parcialmente a barra lateral

--- src/Pages/main/index.tsx
+++ src/Pages/main/index.tsx
@@ -3,16 +3,10 @@ import { useNavigate } from "react-router-dom";
 
 export const MainPage = () => {
   const navigate = useNavigate();
-  const handleLogout = () => {
-    const loginResponse = sessionStorage.getItem("LoginResponseDTO");
-    navigate("/");
-    console.log(loginResponse);
-  };
+
   return (
     <>
-      <div className="flex justify-content-end p-2">
-        <Button icon="pi pi-power-off" rounded onClick={() => handleLogout()} />
-      </div>
+      <div className="flex justify-content-end p-2"></div>
       <h1>mainPage</h1>
     </>
   );
--- src/Pages/menu/index.tsx
+++ src/Pages/menu/index.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { Route, Routes } from "react-router-dom";
 import { SidebarComponent } from "../../Components/Layout/sidebar";
+import { Topbar } from "../../Components/Layout/topbar";
 import { AgendamentosPage } from "../agendamentos";
 import { MainPage } from "../main";
 import { PacientesPage } from "../pacientes";
@@ -9,12 +10,20 @@ export const MenuPage = () => {
   return (
     <div className="flex">
       <SidebarComponent />
-      <div style={{ width: "calc(100vw - 250px)", height: "100vh" }}>
-        <Routes>
-          <Route path="/main" element={<MainPage />} />
-          <Route path="/agendamentos" element={<AgendamentosPage />} />
-          <Route path="/pacientes" element={<PacientesPage />} />
-        </Routes>
+      <div className="flex-col">
+        <Topbar />
+        <div
+          style={{
+            width: "calc(100vw - 250px)",
+            height: "calc(100vh - 47.2px - 1rem)",
+          }}
+        >
+          <Routes>
+            <Route path="/main" element={<MainPage />} />
+            <Route path="/agendamentos" element={<AgendamentosPage />} />
+            <Route path="/pacientes" element={<PacientesPage />} />
+          </Routes>
+        </div>
       </div>
     </div>
   );
